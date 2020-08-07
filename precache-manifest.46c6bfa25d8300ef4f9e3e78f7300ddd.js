2429abfb9536ea675d5482ea85399abef07e5e3b
Daman Sharma <40004503+damansharma006@users.noreply.github.com>
2020-08-08 03:18:47 +0530

    Updates

---
+++ precache-manifest.46c6bfa25d8300ef4f9e3e78f7300ddd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d916589d0416aed93488e2470a9f7f93",
+    "url": "/MyFirstReactApp/index.html"
+  },
+  {
+    "revision": "689de55aeb5d4e59b6e1",
+    "url": "/MyFirstReactApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8f3eaf39661826affbab",
+    "url": "/MyFirstReactApp/static/css/main.ef192cf3.chunk.css"
+  },
+  {
+    "revision": "689de55aeb5d4e59b6e1",
+    "url": "/MyFirstReactApp/static/js/2.270c2e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyFirstReactApp/static/js/2.270c2e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3eaf39661826affbab",
+    "url": "/MyFirstReactApp/static/js/main.92b9bd75.chunk.js"
+  },
+  {
+    "revision": "6dac3fc5c0c0568c3bfb",
+    "url": "/MyFirstReactApp/static/js/runtime-main.fbb343c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/MyFirstReactApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
